33f98eec86c27c2a727680f1d1d4ddc3e2c31a29
Briam David Mora Villalobos <briamtec@gmail.com>
2025-08-22 19:17:16 -0600

    fix: simple aframe settings

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import './aframe/ScaleAnimator.jsx';
 import './aframe/HoverAnimator.jsx';
 import './aframe/GLTFMaterialFix.jsx';
 import './aframe/VideoBackground.jsx';
-import './aframe/PerformanceOptimizer.jsx';
 import content from './content.json';
 import { DEFAULT_DISTANCE_FROM_USER } from './constants.js';
 import { AudioProvider } from './contexts/AudioContext.jsx';
@@ -241,35 +240,34 @@ export function App() {
         </div>}
         <a-scene loading-screen="dotsColor: #ffc21f; backgroundColor: #0071ce" 
                  device-orientation-permission-ui="allowButtonText: Permitir; denyButtonText: Denegar; deviceMotionMessage: Esta Web Inmersiva requiere acceso a los sensores de movimiento de tu dispositivo"
-                 renderer="alpha: false; antialias: false; premultipliedAlpha: true; logarithmicDepthBuffer: false; colorManagement: true; precision: medium; stencil: false; depth: true; powerPreference: low-power"
-                 webgl="antialias: false; alpha: false; depth: true; stencil: false; preserveDrawingBuffer: false; logarithmicDepthBuffer: false; powerPreference: low-power"
+                 renderer="precision: medium; antialias: false; alpha: false; stencil: false; depth: true; logarithmicDepthBuffer: false"
+                 webgl="antialias: false; alpha: false; depth: true; stencil: false; preserveDrawingBuffer: false; logarithmicDepthBuffer: false; powerPreference: default; failIfMajorPerformanceCaveat: false"
                  onerror="console.error('A-Frame error:', event.detail)"
                  embedded="true"
-                 video-bg
-                 performance-optimizer="targetFPS: 30; reduceTextures: true; disableShadows: true; optimizeMaterials: true">
+                 video-bg>
           <a-camera look-controls="reverseMouseDrag: true; mouseEnabled: true; touchEnabled: true" wasd-controls-enabled="false" position="0 0 0"></a-camera>
           <a-entity id="raycaster" raycaster="objects: .clickable" cursor="rayOrigin: mouse"></a-entity>
 
           {/* Load assets */}
           <a-assets timeout="30000">
             <img id="panorama" src="panorama.webp" crossOrigin="anonymous" />
-            {content.images.map(image => <img key={image.id} id={image.id} src={image.src} crossOrigin="anonymous" preload="auto" />)}
-            {content.memory.map(card => <img key={card.id} id={card.id} src={card.src} crossOrigin="anonymous" preload="auto" />)}
-            {content.memory_win.map(card => <img key={card.id} id={card.id} src={card.src} crossOrigin="anonymous" preload="auto" />)}
-            {content.globos.map(globo => <img key={globo.id} id={globo.id} src={globo.src} crossOrigin="anonymous" preload="auto" />)}
-            {content.plataforma.map(plataforma => <img key={plataforma.id} id={plataforma.id} src={plataforma.src} crossOrigin="anonymous" preload="auto" />)}
-            <img id="light-arrow" src="icono.png" crossOrigin="anonymous" preload="auto" />
-            <img id="play-button" src="play.png" crossOrigin="anonymous" preload="auto" />
-            <img id="pantalla-carga" src="walmart_start.png" crossOrigin="anonymous" preload="auto" />
-            <img id="celular" src="celular.png" crossOrigin="anonymous" preload="auto" />
-            <img id="celular-flechas" src="celular-flechas.png" crossOrigin="anonymous" preload="auto" />
-            <img id="celular-instrucciones" src="celular-instrucciones.png" crossOrigin="anonymous" preload="auto" />
-            <img id="next" src="next.png" crossOrigin="anonymous" preload="auto" />
-            <img id="prev" src="prev.png" crossOrigin="anonymous" preload="auto" />
-            <img id="close" src="close.png" crossOrigin="anonymous" preload="auto" />
-            <img id="cardback" src="cardback.png" crossOrigin="anonymous" preload="auto" />
-            <video id="video" src="video.mp4" autoPlay={false} loop={false} preload="metadata" controls={false}></video>
-            <audio id="click" src="click.wav" preload="auto"></audio>
+            {content.images.map(image => <img key={image.id} id={image.id} src={image.src} crossOrigin="anonymous" preload="none" />)}
+            {content.memory.map(card => <img key={card.id} id={card.id} src={card.src} crossOrigin="anonymous" preload="none" />)}
+            {content.memory_win.map(card => <img key={card.id} id={card.id} src={card.src} crossOrigin="anonymous" preload="none" />)}
+            {content.globos.map(globo => <img key={globo.id} id={globo.id} src={globo.src} crossOrigin="anonymous" preload="none" />)}
+            {content.plataforma.map(plataforma => <img key={plataforma.id} id={plataforma.id} src={plataforma.src} crossOrigin="anonymous" preload="none" />)}
+            <img id="light-arrow" src="icono.png" crossOrigin="anonymous" preload="none" />
+            <img id="play-button" src="play.png" crossOrigin="anonymous" preload="none" />
+            <img id="pantalla-carga" src="walmart_start.png" crossOrigin="anonymous" preload="none" />
+            <img id="celular" src="celular.png" crossOrigin="anonymous" preload="none" />
+            <img id="celular-flechas" src="celular-flechas.png" crossOrigin="anonymous" preload="none" />
+            <img id="celular-instrucciones" src="celular-instrucciones.png" crossOrigin="anonymous" preload="none" />
+            <img id="next" src="next.png" crossOrigin="anonymous" preload="none" />
+            <img id="prev" src="prev.png" crossOrigin="anonymous" preload="none" />
+            <img id="close" src="close.png" crossOrigin="anonymous" preload="none" />
+            <img id="cardback" src="cardback.png" crossOrigin="anonymous" preload="none" />
+            <video id="video" src="video.mp4" autoPlay={false} loop={false} preload="none" controls={false}></video>
+            <audio id="click" src="click.wav" preload="none"></audio>
             {content.audios.station_2.map(audio => <audio key={audio.id} id={audio.id} src={audio.src} preload="none"></audio>)}
             {content.audios.station_3.map(audio => <audio key={audio.id} id={audio.id} src={audio.src} preload="none"></audio>)}
             {content.audios.station_4.map(audio => <audio key={audio.id} id={audio.id} src={audio.src} preload="none"></audio>)}
@@ -279,7 +277,7 @@ export function App() {
           </a-assets>
 
           {/* Sky with panorama - fallback when camera is not available */}
-          <a-sky src="#panorama" material="shader: flat"></a-sky>
+          <a-sky src="#panorama"></a-sky>
 
           <a-plane
             class="clickable"
@@ -287,7 +285,6 @@ export function App() {
             position={`0 0 -${DEFAULT_DISTANCE_FROM_USER}`}
             transparent="true"
             scale="1 2 1"
-            material="shader: flat"
             onClick={(e) => {
               setStarted(true);
               e.target.setAttribute("scale", "0 0 0")
